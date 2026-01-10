7b424e0f0524cec8253868d0d7e0989027e8a6d5
Manonraj <manonraj@itgostack.com>
2026-01-10 15:59:25 +0530

    add doctype for modified

---
+++ ch_erp15/ch_erp15/doctype/company_dialog/test_company_dialog.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Admin and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCompanyDialog(FrappeTestCase):
+	pass
